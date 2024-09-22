8e094b12b0d132bd869eeb6f795805a98bea13d1
Daniel <daniborgesmarque@gmail.com>
2024-09-22 20:04:25 -0300

    ADD: finished inventory CRUD

--- site-back-end/Controllers/InventoryController.cs
+++ site-back-end/Controllers/InventoryController.cs
@@ -38,4 +38,88 @@ public class InventoryController: ControllerBase
         List<ListCosmeticsResponse> ListaFiltrada = _mapper.Map<List<ListCosmeticsResponse>>(ListaGerada); 
         return Results.Ok(ListaFiltrada);
     }
+
+    [HttpGet("list_applied")]
+    public IResult ListApplied ([FromBody] string userId)
+    {
+        //pega as informações de inventario do usuario
+        var inventarioEncontrado = _ludocontext.UserCosmetics.Where(c => c.user_id == userId).FirstOrDefault();
+        if(inventarioEncontrado == null)
+        {
+            return Results.Problem("inventario não encontrado");
+        }
+
+        List<string> listaDeIds = inventarioEncontrado.applied_cosmetics;
+
+        // depois com esses ids, capturo todos os cosmeticos da lista de cosmeticos
+        List<Cosmetic> ListaGerada = _ludocontext.Cosmetics.Where(e => listaDeIds.Contains(e.id)).ToList();
+        List<ListCosmeticsResponse> ListaFiltrada = _mapper.Map<List<ListCosmeticsResponse>>(ListaGerada); 
+        return Results.Ok(ListaFiltrada);
+    }
+
+    [HttpPost("apply")]
+    public IResult ApplyCosmetic([FromBody] ApplyCosmeticBody jsonBody)
+    {
+        //pega as informações de inventario do usuario
+        var inventarioEncontrado = _ludocontext.UserCosmetics.Where(c => c.user_id == jsonBody.Userid).FirstOrDefault();
+        if(inventarioEncontrado == null)
+        {
+            return Results.Problem("inventario não encontrado");
+        }
+        //checa se o usuario possui o cosmético no seu inventario
+        if (!inventarioEncontrado.available_cosmetics.Contains(jsonBody.CosmeticId))
+        {
+            return Results.Unauthorized();
+        }
+        //checa se o usuario ja tem o cosmetico aplicado 
+        if (inventarioEncontrado.applied_cosmetics.Contains(jsonBody.CosmeticId))
+        {
+            return Results.Conflict("usuario ja aplicou esse cosmetico!");
+        }
+        //pega informações do cosmetico selecionado
+        var cosmeticoEncontrado = _ludocontext.Cosmetics.Where(c => c.id == jsonBody.CosmeticId).FirstOrDefault();
+        if (cosmeticoEncontrado == null)
+        {
+            return Results.NotFound("O cosmético a ser aplicado não foi encontrado!");
+        }
+        
+        //checa se o usuario tem um cosmetico aplicado do mesmo tipo
+        List<string> listaDeIds = inventarioEncontrado.applied_cosmetics;
+        Cosmetic cosmeticosIgual = _ludocontext.Cosmetics.Where(c => c.item_type == cosmeticoEncontrado.item_type && listaDeIds.Contains(c.id)).FirstOrDefault();
+
+        //se tiver, removemos ele da lista de cosmeticos aplicados
+        if (cosmeticosIgual != null)
+        {
+            inventarioEncontrado.applied_cosmetics.Remove(cosmeticosIgual.id);   
+        }
+        //finalmente, aplicamos o cosmético ao usuário
+        inventarioEncontrado.applied_cosmetics.Add(cosmeticoEncontrado.id);
+        _ludocontext.UserCosmetics.Update(inventarioEncontrado);
+        _ludocontext.SaveChanges();
+        
+        return Results.Ok();  
+    }
+
+    [HttpPost("unnapply")]
+    public IResult UnnapplyCosmetic ([FromBody] ApplyCosmeticBody jsonBody)
+    {
+        //pega as informações de inventario do usuario
+        var inventarioEncontrado = _ludocontext.UserCosmetics.Where(c => c.user_id == jsonBody.Userid).FirstOrDefault();
+        if(inventarioEncontrado == null)
+        {
+            return Results.Problem("inventario não encontrado");
+        }
+        //checa se o cosmetico está aplicado ao usuario
+        if(!inventarioEncontrado.applied_cosmetics.Contains(jsonBody.CosmeticId))
+        {
+            return Results.NotFound("esse cosmético não está aplicado");
+        }
+
+        //finalmente, remove o cosmeticos dos items aplicados
+        inventarioEncontrado.applied_cosmetics.Remove(jsonBody.CosmeticId);
+        _ludocontext.UserCosmetics.Update(inventarioEncontrado);
+        _ludocontext.SaveChanges();
+
+        return Results.Ok();  
+    }
 }
--- site-back-end/Models/ListInventoryBody.cs
+++ site-back-end/Models/ListInventoryBody.cs
@@ -1,11 +1,12 @@
 using System.Text.Json.Serialization;
 
 public class ListInventoryBody {
-    [JsonPropertyName("userid")]
+    [JsonPropertyName("user_id")]
     public string UserId { get; set;}
 
     [JsonPropertyName("page")]
     public int Page { get; set; }
+    
     [JsonPropertyName("items_per_page")]
     public int ItemsPerPage { get; set; }
 }
