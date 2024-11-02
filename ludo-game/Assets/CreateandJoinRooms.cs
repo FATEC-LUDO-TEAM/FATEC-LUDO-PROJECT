cb4bde62961de64b2575f219f4bac36e1c07e489
Arthur-Heinrichs <contato.arthurheinrichs@gmail.com>
2024-11-02 13:50:08 -0300

    Inclusão do Photon, Telas de Loading e Lobby

---
+++ ludo-game/Assets/CreateandJoinRooms.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+
+public class CreateandJoinRooms : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    public InputField createInput;
+    public InputField joinInput;
+    
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(createInput.text);
+
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinInput.text);
+
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Ludo");
+    }
+
+}
