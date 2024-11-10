f3ca874fc5946ef9e02804319dde88df25e04845
Arthur-Heinrichs <contato.arthurheinrichs@gmail.com>
2024-11-10 12:26:40 -0300

    Fiz merda. Tá arrumado.

--- ludo-game/Assets/Scripts/GameScript.cs
+++ ludo-game/Assets/Scripts/GameScript.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
@@ -1118,6 +1118,7 @@ public class GameScript : MonoBehaviour {
 
 		switch (MainMenuScript.howManyPlayers) 
 		{
+			
 			case 2:
 				if (playerTurn == "RED") 
 				{
